bf13cc5fb06ec9b2fcd46fc07ed6b60c95c65a8f
Abdeladim <abdeladimdemnati3@gmail.com>
2026-07-23 12:33:53 +0100

    Fix STATIC_ROOT to use frontend/build/static which exists in Heroku slug

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -123,16 +123,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'frontend' / 'build' / 'static'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-_FRONTEND_BUILD_STATIC = BASE_DIR / 'frontend' / 'build' / 'static'
-if _FRONTEND_BUILD_STATIC.exists():
-    STATICFILES_DIRS.append(_FRONTEND_BUILD_STATIC)
-
-WHITENOISE_USE_FINDERS = True
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'static'
